c28fd853294a7ae4aebf02da589135f29341b557
judyatohio <js467820@ohio.edu>
2023-02-01 01:29:54 -0500

    secret

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
 "use strict"
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/club-logo.png") {
+    myImage.setAttribute("src", "images/jerryed.jpeg");
+  } else {
+    myImage.setAttribute("src", "images/club-logo.png");
+  }
+};
