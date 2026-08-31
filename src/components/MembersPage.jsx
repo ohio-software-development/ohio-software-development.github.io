1b86b8b7ad98de744273fe0c7e2d5d86358c61ec
Thomas Perkins <thomasperkins26m@gmail.com>
2026-08-31 18:30:07 -0400

    add thomas

--- src/components/MembersPage.jsx
+++ src/components/MembersPage.jsx
@@ -16,6 +16,7 @@ import brendan_avatar from "../assets/Brendan-Smyers.jpg";
 import virginia_avatar from "../assets/virginia.png";
 import tristan_avatar from "../assets/tristan.jpeg"
 import NathanB_avatar from "../assets/Nathan.jpeg"
+import thomas_avatar from "../assets/thomas.jpeg"
 
 /**
  * TO ADD A MEMBER
@@ -154,6 +155,13 @@ const memberData = [
     "role": "member",
     "github": "https://github.com/il225",
     "avatar": {},
+  },
+  {
+    "id": 20,
+    "name": "Thomas Perkins",
+    "role": "member",
+    "github": "https://github.com/tperkins26",
+    "avatar": thomas_avatar.jpg,
   }
 
   // Add more members as needed
